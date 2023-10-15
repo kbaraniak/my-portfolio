9d1fe835bf692a5ba1c7de5e9d54092981ad5419
Klubuntu <local@fix.pc>
2023-10-15 20:03:51 +0200

    Fileloader added for static elements

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    webpack: (config, options) => {
+        config.module.rules.push({
+            test: /\.(jpe?g|png|svg|gif|ico|eot|ttf|woff|woff2|mp4|pdf|webm|txt)$/,
+            type: 'asset/resource',
+            generator: {
+                filename: 'static/chunks/[path][name].[hash][ext]'
+            },
+        });
+
+        return config;
+    }
+}
 
 module.exports = nextConfig
