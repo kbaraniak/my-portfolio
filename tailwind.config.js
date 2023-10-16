aae75a3e651c023f59235bbc4b9d538329354a4b
Klubuntu <local@fix.pc>
2023-10-16 20:39:41 +0200

    Update content to support png files

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx,png}',
   ],
   theme: {
     extend: {
