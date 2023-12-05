6446ddc3ec7849f403ac3ac5eb1ef9f3b3f03198
Kamil Baraniak <90936580+kbaraniak@users.noreply.github.com>
2023-12-05 07:55:38 +0100

    Update Meta Tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,23 +8,23 @@ export default function Home() {
   return <>
     <Head>
       <title>Kamil Baraniak - My Projects</title>
-      <meta name="description" content="Portfolio of my work">
+      <meta name="description" content="Portfolio of my work" />
       <link rel="icon" type="image/png" href="/favicon/folder.png" />
     
       <!-- Facebook/Discord Tags -->
-      <meta property="og:url" content="https://kbaraniak.vercel.app">
-      <meta property="og:type" content="website">
-      <meta property="og:title" content="Kamil Baraniak - My Projects">
-      <meta property="og:description" content="Portfolio of my work">
-      <meta property="og:image" content="/favicon/share.png">
+      <meta property="og:url" content="https://kbaraniak.vercel.app" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="Kamil Baraniak - My Projects" />
+      <meta property="og:description" content="Portfolio of my work" />
+      <meta property="og:image" content="/favicon/share.png" />
     
       <!-- Twitter Tags -->
-      <meta name="twitter:card" content="summary_large_image">
-      <meta property="twitter:domain" content="kbaraniak.vercel.app">
-      <meta property="twitter:url" content="https://kbaraniak.vercel.app">
-      <meta name="twitter:title" content="Kamil Baraniak - My Projects">
-      <meta name="twitter:description" content="Portfolio of my work">
-      <meta name="twitter:image" content="/favicon/share.png">
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="twitter:domain" content="kbaraniak.vercel.app" />
+      <meta property="twitter:url" content="https://kbaraniak.vercel.app" />
+      <meta name="twitter:title" content="Kamil Baraniak - My Projects" />
+      <meta name="twitter:description" content="Portfolio of my work" />
+      <meta name="twitter:image" content="/favicon/share.png" />
     </Head>
     <div>
       <Header />
