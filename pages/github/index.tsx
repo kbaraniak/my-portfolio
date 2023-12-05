2c174578373a3421509be6c8c6b30ef081224142
Kamil Baraniak <90936580+kbaraniak@users.noreply.github.com>
2023-12-05 08:15:59 +0100

    Update Meta Tags

--- pages/github/index.tsx
+++ pages/github/index.tsx
@@ -11,20 +11,20 @@ export default function Home() {
       <meta name="description" content="Portfolio of my work" />
       <link rel="icon" type="image/png" href="/favicon/github_clean.png" />
     
-      <{/* Facebook/Discord Tags */}
-      <meta property="og:url" content="https://kbaraniak.vercel.app">
-      <meta property="og:type" content="website">
-      <meta property="og:title" content="Kamil Baraniak - My Projects">
-      <meta property="og:description" content="Portfolio of my work">
-      <meta property="og:image" content="/favicon/share.png">
+      {/* Facebook/Discord Tags */}
+      <meta property="og:url" content="https://kbaraniak.vercel.app"/>
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="Kamil Baraniak - My Projects" />
+      <meta property="og:description" content="Portfolio of my work" />
+      <meta property="og:image" content="/favicon/share.png" />
     
       {/* Twitter Tags */}
-      <meta name="twitter:card" content="summary_large_image">
-      <meta property="twitter:domain" content="kbaraniak.vercel.app">
-      <meta property="twitter:url" content="https://kbaraniak.vercel.app">
-      <meta name="twitter:title" content="Kamil Baraniak - My Projects">
-      <meta name="twitter:description" content="Portfolio of my work">
-      <meta name="twitter:image" content="/favicon/share.png">
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="twitter:domain" content="kbaraniak.vercel.app" />
+      <meta property="twitter:url" content="https://kbaraniak.vercel.app" />
+      <meta name="twitter:title" content="Kamil Baraniak - My Projects" />
+      <meta name="twitter:description" content="Portfolio of my work" />
+      <meta name="twitter:image" content="/favicon/share.png" />
     </Head>
     <div>
       <Header />
