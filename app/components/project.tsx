7904355a66dfa07b5c85141b27279be98397df96
Kamil Baraniak <90936580+kbaraniak@users.noreply.github.com>
2024-06-01 01:33:02 +0200

    Make project component

---
+++ app/components/project.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Project(projectName: string, projectID: string, projectDescription: string, projectType: string, projectUrl: string){
+  return (
+     <div className="project" id={projectID}>
+            <div className="project-img w-70 h-60 bg-transparent mx-3 my-2 rounded-lg flex justify-center items-center">
+               <div id="frame" className="social">
+                  <div id="frame-header">
+                     <h2 className="font-itim">{projectType}</h2>
+                  </div>
+               </div>
+            </div>
+            <h1 className='text-center cursor-pointer hover:text-amber-100'>{projectUrl}</h1>
+            <div className="badges flex justify-center items-center">
+               <div className="html"></div>
+               <div className="css"></div>
+               <div className="js"></div>
+            </div>
+     </div>
+  )
+}
+
+export default function Project;
