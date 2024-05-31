8d13c844702f47908138f93c7fedd48f2153720b
Kamil Baraniak <90936580+kbaraniak@users.noreply.github.com>
2024-06-01 01:16:29 +0200

    Removed abandoned project

--- app/components/my_projects.tsx
+++ app/components/my_projects.tsx
@@ -62,21 +62,6 @@ const Projects = () => {
                <div className="js"></div>
             </div>
          </div>
-         <div className="project" id="project3">
-            <div className="project-img w-70 h-60 bg-transparent mx-3 my-2 rounded-lg flex justify-center items-center">
-               <div id="frame" className="npws">
-                  <div id="frame-header">
-                     <h2>Cloud Service</h2>
-                  </div>
-               </div>
-            </div>
-            <h1 className='text-center cursor-pointer hover:text-amber-100'>npws.cloud</h1>
-            <div className="badges flex justify-center items-center">
-               <div className="html"></div>
-               <div className="css"></div>
-               <div className="js"></div>
-            </div>
-         </div>
          <div className="project" id="project4">
             <div className="project-img w-70 h-60 bg-transparent mx-3 my-2 rounded-lg flex justify-center items-center">
                <div id="frame" className="oneros">
