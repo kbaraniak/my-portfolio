d8f980f5ccd80dfc72c164b37610f4d446b4607a
Kamil Baraniak <90936580+kbaraniak@users.noreply.github.com>
2024-07-19 01:28:11 +0200

    Temporary Page

---
+++ src/app/page.js
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+
+      </div>
+
+      <div className={styles.center}>
+        <h3>Page under contruction</h3>
+        <h5>Please wait to next update</h5>
+      </div>
+
+      <div className={styles.grid}>
+
+      </div>
+    </main>
+  );
+}
